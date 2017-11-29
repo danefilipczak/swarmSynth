f1b486f676683b207d829bedec21dfa5cdbba22e
Dane Filipczak <dane@imaginationrock.com>
2017-11-29 14:43:03 -0500

    completed piano interactivity

--- keyboard.js
+++ keyboard.js
@@ -3,14 +3,30 @@ function WhiteKey(x_, y_, w_, h_) {
 	this.y = y_;
 	this.w = w_;
 	this.h = h_;
+	this.over = false;
+	this.active = false;
 
 	this.display = function() {
 		p5.rectMode(p5.CORNER)
 		// p5.fill(255, 255, 100, 100)
-		p5.noFill()
+		
 		p5.stroke(200)
 		p5.strokeWeight(1)
+		this.over ? p5.fill(255, 35, 35) : p5.noFill();
+		if(this.active){p5.fill(0, 255, 255)}
 		p5.rect(this.x, this.y, this.w, this.h-1)
+		this.over=false;
+	}
+
+	this.checkOver = function(){
+		// this.over = false;
+		x = p5.mouseX;
+		y = p5.mouseY;
+		if(x>this.x&&x<this.x+this.w&&y>this.y&&y<this.y+this.h){
+			this.over = true;
+			console.log('over')
+		}
+		return this.over;
 	}
 
 }
@@ -20,6 +36,8 @@ function BlackKey(x_, y_, w_, h_) {
 	this.y = y_;
 	this.w = w_;
 	this.h = h_;
+	this.active = false;
+	this.over = false;
 
 	this.display = function() {
 		// p5.rectMode(p5.CENTER)
@@ -27,8 +45,20 @@ function BlackKey(x_, y_, w_, h_) {
 		p5.noFill()
 		p5.stroke(0)
 		p5.strokeWeight(1)
-		p5.fill(25)
+		this.over ? p5.fill(255, 35, 35) : p5.fill(25);
+		if(this.active){p5.fill(0, 255, 255)}
 		p5.rect(this.x, this.y, this.w-5, this.h-1)
+		this.over=false;
+	}
+
+	this.checkOver = function(){
+		// this.over=false;
+		x = p5.mouseX;
+		y = p5.mouseY;
+		if(x>this.x&&x<this.x+this.w&&y>this.y&&y<this.y+this.h){
+			this.over = true;
+		}
+		return this.over;
 	}
 
 }
@@ -46,10 +76,20 @@ function Keyboard(x_, y_, width_, height_, onClick) {
 
 }
 
+Keyboard.prototype.update = function(){
+	for(var i = 0; i<this.keys.length; i++){
+		if(this.keys[i].checkOver()){
+			currentOver = this.keys[i];
+			break;
+		}
+		currentOver=null;
+	}
+}
+
 Keyboard.prototype.display = function() {
-	this.keys.forEach(function(k) {
-		k.display()
-	})
+	for(var i = this.keys.length-1; i>=0; i--){
+		this.keys[i].display();
+	}
 }
 
 Keyboard.prototype.populate = function(){
@@ -59,17 +99,17 @@ Keyboard.prototype.populate = function(){
 	var w = 40;
 	var remainder = this.width%w;
 	while((i+1)*w<this.width){
-		whiteKeys.push(new WhiteKey(w*i+(remainder/2), 0, w, this.height))
+		whiteKeys.push(new WhiteKey(this.x+(w*i+(remainder/2)), 0, w, this.height))
 		i++;
 	}
 
 	var j = 0;
 	while((j+2)*w<this.width){
 		if(j%7!=2&&j%7!=6){
-			blackKeys.push(new BlackKey(w*j+(remainder/2)+(w/2), 0, w, this.height*0.618))
+			blackKeys.push(new BlackKey(this.x+(w*j+(remainder/2)+(w/2)), 0, w, this.height*0.618))
 			
 		}
 		j++;
 	}
-	this.keys = whiteKeys.concat(blackKeys)
+	this.keys = blackKeys.concat(whiteKeys)
 }
--- sketch.js
+++ sketch.js
@@ -1,21 +1,43 @@
 
-
+var currentOver;
+var currentActive;
 
 var sketch = function (p5) {
 	var keyboard;
 	p5.setup = function(){
-		c = p5.createCanvas(window.innerWidth, 150);
-    	c.position(0, window.innerHeight-200)
+		c = p5.createCanvas(window.innerWidth, 100);
+    	c.position(0, window.innerHeight-100-(100*0.318))
     	p5.background(10, 100)
-    	keyboard = new Keyboard(0, window.innerHeight-200, p5.width, p5.height, function(n){
+    	var width = p5.width;
+    	if(width>1200){width=1200};
+    	keyboard = new Keyboard((p5.width-width)/2, window.innerHeight-200, width, p5.height, function(n){
     		setTargets(n);
     	})
+    	p5.frameRate(24)
+    	currentActive = keyboard.keys[0];
+    	keyboard.keys[0].active=true;
 	}
 
 	p5.draw = function(){
 		p5.clear()
 		// p5.background(10, 100)
+
 		keyboard.display()
+		keyboard.update()
+	}
+
+	p5.mousePressed = function(){
+
+		if(currentOver){
+		if(currentActive!==currentOver){
+			console.log('different')
+			currentActive.active=false
+		}
+		currentActive = currentOver;
+		currentActive.active=true;
+		}
+
+
 	}
 
 }
